b40987a10b7cd6efa9b23bb5ca2e16cf60ece546
Samir6467ff <kariokie01@gmail.com>
2024-06-19 23:17:42 +0300

    Add files via upload

---
+++ plugins/دبدوب.js
@@ -0,0 +1,17 @@
+
+let handler = async (m, { conn, args, 
+text, usedPrefix, command }) => {
+  let too = `[❗] الأمر خطأ *🕵🏻‍♂️*\n\n *مـثــال* :\n*${usedPrefix + command}* تربو عمك`
+
+  if (!text) throw too
+  if (!text.includes('+')) throw too  
+  let [a, b] = text.split`+`   
+  let lr = (`https://api.popcat.xyz/pooh?text1=${a}&text2=${b}`)
+  conn.sendFile(m.chat, lr, 'drake.png', `تم بواسطه ✅
+  𝐵𝛩𝑺𝑺🐦‍🔥`, m)
+}
+handler.help = ['drake']
+handler.tags = ['maker']
+handler.command = ['تربو3','دبدوب']
+
+export default handler
